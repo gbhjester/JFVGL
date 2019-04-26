c82e0d1ada1de0da605e4d1cf30b1dd7dfbc14f3
Jester <11262250+gbhjester@users.noreply.github.com>
2019-04-25 21:03:37 -0400

    2019-04-25 Add include header stb_image.h Add test OpenGL rendering

--- JFVGL/src/GLUTWindow.cpp
+++ JFVGL/src/GLUTWindow.cpp
@@ -9,6 +9,8 @@
  * Created on April 25, 2019, 1:57 AM
  */
 
+#include <cmath>
+#include <cstdio>
 #include "GLUTWindow.h"
 
 GLUTWindow::GLUTWindow(){ }
@@ -25,8 +27,14 @@ GLUTWindow &GLUTWindow::operator=(GLUTWindow rhs)
 
 /* API */
 
+float s(float p, float t)
+{
+	return p*t;
+}
+
 void GLUTWindow::Start(int argc, char **argv)
 {
+	static float f = 1.f;
 	glutInit(&argc, argv);
 	glutInitWindowSize(
 		glutGet(GLUT_SCREEN_WIDTH) / 3,
@@ -34,23 +42,55 @@ void GLUTWindow::Start(int argc, char **argv)
 	glutInitWindowPosition(
 		(glutGet(GLUT_SCREEN_WIDTH) - glutGet(GLUT_INIT_WINDOW_WIDTH)) / 2,
 		(glutGet(GLUT_SCREEN_HEIGHT) - glutGet(GLUT_INIT_WINDOW_HEIGHT)) / 2);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE /*| GLUT_MULTISAMPLE*/);
 	glutCreateWindow("JFVGL");
 	glutDisplayFunc([]()
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 		glBegin(GL_POLYGON);
-		glVertex2f(-0.5, -0.5);
+		float t = f;
+		float x = -0.5f;
+		float y = -0.5f;
+		glVertex2f(s(x, t), s(y, t));
+		x = -0.5f;
+		y = 0.5f;
+		glVertex2f(s(x, t), s(y, t));
+		x = 0.5f;
+		y = 0.5f;
+		glVertex2f(s(x, t), s(y, t));
+		x = 0.5f;
+		y = -0.5f;
+		glVertex2f(s(x, t), s(y, t));
+		/*glVertex2f(-0.5, -0.5);
 		glVertex2f(-0.5, 0.5);
 		glVertex2f(0.5, 0.5);
-		glVertex2f(0.5, -0.5);
+		glVertex2f(0.5, -0.5);*/
 		glEnd();
 		glFlush();
+	});
+	glutMouseFunc([](int btn, int state, int x, int y)
+	{
+		if (state == 1)
+			return;
+		float s = 0.1f;
+		if (btn == GLUT_LEFT_BUTTON)
+		{
+			f += s;
+		}
+		else if (btn == GLUT_RIGHT_BUTTON)
+		{
+			f -= s;
+		}
+		glutPostRedisplay();
+		printf("%f\n", f);
 	});
 	glClearColor(0.25f, 0.25f, 0.25f, 1.f);
 	glColor3f(0.5f, 0.5f, 1.f);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
-	glutMainLoop();
+	while (true)
+	{
+		glutMainLoopEvent();
+	}
 }
--- JFVGL/src/Main.cpp
+++ JFVGL/src/Main.cpp
@@ -6,13 +6,138 @@
  * Main.c
  */
 
-//#include <stdio.h>
+#include <cstdlib>
 #include "main.h"
 #include "GLUTWindow.h"
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_FAILURE_USERMSG
+#include "stb_image.h"
+
+unsigned int texid;
+unsigned char *img;
 
 int main(int argc, char *argv[])
 {
-	GLUTWindow wnd;
+	/*GLUTWindow wnd;
 	wnd.Start(argc, argv);
+	return 0;*/
+	int w, h, ch;
+	//const char *fn = "E:/torrents/updated mosiac.png";
+	//const char *fn = "table.jpg";w = 3840;h = 2160;
+	const char *fn = "nrm.png";w = 1024;h = 1024;
+	//const char *fn = "table2.png";w = 375;h = 375;
+	img = stbi_load(fn, &w, &h, &ch, 3);
+	if (img)
+	{
+		const char *format = NULL;
+		switch (ch)
+		{
+		case 1:
+			format = "g";
+			break;
+		case 2:
+			format = "ga";
+			break;
+		case 3:
+			format = "rgb";
+			break;
+		case 4:
+			format = "rgba";
+			break;
+		default:
+			format = "Unknown";
+		}
+		printf("Loaded image %s\nWidth : %d\nHeight : %d\nFormat : %s\n", fn, w, h, format);
+	}
+	else
+	{
+		printf("Image failed to load : %s (%s)\n", stbi_failure_reason(), strerror(errno));
+		return -1;
+	}
+	static float f = 1.f;
+	glutInit(&argc, argv);
+	glutInitWindowSize(
+		glutGet(GLUT_SCREEN_WIDTH) / 3,
+		glutGet(GLUT_SCREEN_HEIGHT) / 2);
+	glutInitWindowPosition(
+		(glutGet(GLUT_SCREEN_WIDTH) - glutGet(GLUT_INIT_WINDOW_WIDTH)) / 2,
+		(glutGet(GLUT_SCREEN_HEIGHT) - glutGet(GLUT_INIT_WINDOW_HEIGHT)) / 2);
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE /*| GLUT_MULTISAMPLE*/);
+	glutCreateWindow("JFVGL");
+	glutDisplayFunc([]()
+	{
+		float v[] = {
+			-0.5f, 0.5f,
+			0.5f, 0.5f,
+			-0.5f, -0.5f,
+			0.5f, -0.5f
+		};
+		unsigned short vi[] = {0, 1, 2, 2, 3, 1};
+		float uv[] = {
+			0.f, 0.f,
+			1.f, 0.f,
+			0.f, 1.f,
+			1.f, 1.f
+		};
+		glClear(GL_COLOR_BUFFER_BIT);
+		glVertexPointer(2, GL_FLOAT, 0, v);
+		glTexCoordPointer(2, GL_FLOAT, 0, uv);
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+		glBindTexture(GL_TEXTURE_2D, texid);
+		glEnable(GL_TEXTURE_2D);
+		glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+		glColor3f(1.f, 1.f, 1.f);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, vi);
+		glDisable(GL_TEXTURE_2D);
+		glDisableClientState(GL_VERTEX_ARRAY);
+		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+		glDisable(GL_TEXTURE_2D);
+		glFlush();
+		/*glClear(GL_COLOR_BUFFER_BIT);
+		glDrawPixels(w, h, GL_RGB, GL_UNSIGNED_BYTE, img);*/
+	});
+		/*glClear(GL_COLOR_BUFFER_BIT);
+		glDrawPixels(w, h, GL_RGB, GL_UNSIGNED_BYTE, img);*/
+	glutMouseFunc([](int btn, int state, int x, int y)
+	{
+		if (state == 1)
+			return;
+		float s = 0.1f;
+		if (btn == GLUT_LEFT_BUTTON)
+		{
+			f += s;
+		}
+		else if (btn == GLUT_RIGHT_BUTTON)
+		{
+			f -= s;
+		}
+		glutPostRedisplay();
+		printf("%f\n", f);
+	});
+	glClearColor(0.25f, 0.25f, 0.25f, 1.f);
+	glColor3f(0.5f, 0.5f, 1.f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+	glGenTextures(1, &texid);
+	glBindTexture(GL_TEXTURE_2D, texid);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(
+		GL_TEXTURE_2D,
+		0,
+		GL_RGB,
+		w,
+		h,
+		0,
+		GL_RGB,
+		GL_UNSIGNED_BYTE,
+		img);
+	while (true)
+	{
+		glutMainLoopEvent();
+	}
+	glDeleteTextures(1, &texid);
+	stbi_image_free(img);
 	return 0;
 }
