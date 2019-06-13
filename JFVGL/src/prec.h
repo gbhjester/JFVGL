be5f610ba8c95df7307f2cddf4e732f645423e75
Jester <11262250+gbhjester@users.noreply.github.com>
2019-06-12 22:53:34 -0400

    Add prec.h

---
+++ JFVGL/src/prec.h
@@ -0,0 +1,13 @@
+/*
+ * I, Jester, hereby release this work, except as stated otherwise, into the public domain.
+ */
+
+/**
+ * prec.h
+ */
+
+#include "wx/wx.h"
+#include "wx/display.h"
+#include "wx/frame.h"
+#include "wx/glcanvas.h"
+#include "wx/image.h"
